2492c7d0a227370836810a5fb780a01fa1dcaba2
Sulbha Bansal <sulbha15@gmail.com>
2018-05-30 06:16:42 +0530

    Updated test package and dependencies

--- src/main/java/com/project/twitter/web/SpringBootFirstWebApplication.java
+++ src/main/java/com/project/twitter/web/SpringBootFirstWebApplication.java
@@ -1,21 +1,18 @@
 package com.project.twitter.web;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SuppressWarnings("deprecation")
 @SpringBootApplication
+@EnableAutoConfiguration
 @ComponentScan("com.project.twitter.web")
-public class SpringBootFirstWebApplication extends SpringBootServletInitializer{
+public class SpringBootFirstWebApplication {
 
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SpringBootFirstWebApplication.class);
-    }
-	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootFirstWebApplication.class, args);
 	}
